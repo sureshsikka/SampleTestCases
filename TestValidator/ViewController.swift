6c663fd85fef9cdf77d84410a8db548c4233e972
suresh <suresh@kvana.com>
2017-03-16 17:01:36 +0530

    crasing app

--- TestValidator/ViewController.swift
+++ TestValidator/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let id = Int64.max
+        let predicate = NSPredicate(format: "id == %@", id as NSNumber)
+        print(predicate)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
